d7e8b0fc53dd738637502ad859f866b41c329834
jononekin <jon.iturrioz@ehu.es>
2019-11-26 15:34:35 +0100

    Create PruebaTest3.java

---
+++ iso1/src/test/java/iso1/PruebaTest3.java
@@ -0,0 +1,22 @@
+package iso1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaTest3 {
+
+	@Test
+	public void test() {
+		assertTrue(4==4);
+	}
+	@Test
+	public void test2() {
+		assertTrue(2==2);
+	}
+	@Test
+	public void test3() {
+		assertTrue(3==3);
+	}
+}
+© 2019 GitHub, Inc.
